141a4eadc8c087d630e536f973d2b3fe51253cc1
CharReek <charlottereek@outlook.com>
2021-11-28 14:17:02 +0000

    display addition questiosn to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,19 +10,28 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked Sumbit!");
             } else{
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+    runGame("addition");
+
 })
 
 /**
  * the main game 'loop', called when script if first loaded
  * and after the users answer has been processed
  */
-function runGame(){
-let numb1 = Math.floor(Math.random() *25) +1;
-let numb2 = Math.floor(Math.random() *25) +1;
+function runGame(gameType){
+    let num1 = Math.floor(Math.random() *25) +1;
+    let num2 = Math.floor(Math.random() *25) +1;
+
+    if (gameType === "addition"){ 
+        displayAdditionQuestion(num1, num2)
+    } else {
+        alert(`unknown game type ${gameType}`);
+        throw `unknown game type ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer(){
@@ -38,8 +47,10 @@ function incrementScore(){
 function incrementWrongAnswer(){
 
 }
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 function displaySubtractQuestion(){
     
